661246beecefc9d49244e3ddde5f3a5c26dba50a
zhaozhang <zhangzhao0718@gmail.com>
2023-04-17 21:47:46 -0500

    remove old files

--- src/model/moe/__init__.py
+++
@@ -1,2 +0,0 @@
-from moe.predictor import Predictor
-from moe.predictor import raw_features_to_np_ndarray
--- src/sim/__init__.py
+++
@@ -1,4 +0,0 @@
-from sim.simulator import *
-from sim.job import Job
-from sim.job import JobLog
-from sim.simparser import SimParser
